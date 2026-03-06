7fcd890cddcbd75f4950c528be68815b24c463fe
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2026-03-06 16:02:11 -0300

    mudanças e arrumações

--- Lista Ligada.c
+++ Lista Ligada.c
@@ -16,49 +16,99 @@ typedef struct cabecalho {
     int tamanho;
 }cabecalho;
 
+cabecalho Cria_Lista(cabecalho lista);
 
-void Cria_No_Final(cabecalho lista);
+cabecalho Cria_No_Inicio(cabecalho lista);
 
-void Cria_Lista(cabecalho* lista);
+cabecalho Cria_No_Final(cabecalho lista);
+
+cabecalho Deleta_No(cabecalho lista);
+
+void Print_lista(cabecalho lista);
 
 int main(){
 
-    cabecalho *lista1;
+    cabecalho lista1;
 
-    Cria_Lista(lista1);
+    lista1 = Cria_Lista(lista1);
 
+    lista1 = Cria_No_Inicio(lista1);
+
+    lista1 = Cria_No_Final(lista1);
+
+    Print_lista(lista1);
 
 return 0;
 }
 
-void Cria_No_Final(cabecalho lista){
-    No *aux;
 
-    aux = lista.primeiro;
-
-    while( aux->prox != NULL){
-        aux = aux->prox;
-    }
+cabecalho Cria_Lista(cabecalho lista){
 
     No *novoNo = malloc(sizeof(No));
 
-    // coloco o elemento do no e deixo o ponteiro em null
-    printf("Digite o valor do No ");
-    scanf("%d", novoNo->elemento);
-    novoNo->prox = NULL;
-
-    aux->prox = novoNo;
-}
-
-void Cria_Lista(cabecalho* lista){
-
-    No *novoNo = malloc(sizeof(No));
-
-    printf("Digite o primeiro elemento");
-    scanf("%d",novoNo->elemento);
+    printf("Digite o primeiro elemento ");
+    scanf("%d",&novoNo->elemento);
 
     // defino o primeiro elemento e deixo o No em null
-    lista->primeiro = novoNo;
+    lista.primeiro = novoNo;
     novoNo->prox = NULL;
+
+    //vai definir como o ultimo no tambem e seu tamanho
+    lista.ultimo = novoNo;
+    lista.tamanho = 1;
+
+
+    return lista;
+}
+
+cabecalho Cria_No_Inicio(cabecalho lista){
+
+    //cria um novo nó
+    No *novoNo = malloc(sizeof(No));
+
+    printf("Digite o elemento a ser criado ");
+    scanf("%d",&novoNo->elemento);
+
+    //troca de ponteiros
+    novoNo->prox = lista.primeiro;
+    lista.primeiro = novoNo;
+
+    lista.tamanho++;
+
+    return lista;
+
+}
+
+cabecalho Cria_No_Final(cabecalho lista){
+
+    No *aux;
+
+    //cria um novo nó
+    No *novoNo = malloc(sizeof(No));
+
+    printf("Digite o elemento a ser criado no final ");
+    scanf("%d",&novoNo->elemento);
+
+    // trocar o ponteiro que esta no final pelo novo
+
+    aux = lista.ultimo;
+
+    aux->prox = novoNo;
+    novoNo->prox = NULL;
+
+    lista.ultimo = novoNo;
+
+    lista.tamanho++;
+
+    return lista;
+}
+
+void Print_lista(cabecalho lista){
+    No *aux = lista.primeiro;
+
+    while(aux != NULL){
+        printf(" %d ->", aux->elemento);
+        aux = aux->prox;
+    }
 
 }
